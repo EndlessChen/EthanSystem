1f72161f6b476a81a6e6d3f680c1a86a6075e697
Ethan Chen <zb.StriveChen@gmail.com>
2024-05-15 00:24:58 +0800

    modify CorsConfig

--- src/main/java/com/project/ethansystem/config/CorsConfig.java
+++ src/main/java/com/project/ethansystem/config/CorsConfig.java
@@ -6,13 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://121.41.115.199", "http://8.130.30.154")
-                .allowCredentials(true)
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .maxAge(3600);
-    }
+    // @Override
+    // public void addCorsMappings(CorsRegistry registry) {
+    //     registry.addMapping("/**")
+    //             .allowedOrigins("http://localhost:3000", "http://121.41.115.199", "http://8.130.30.154")
+    //             .allowCredentials(true)
+    //             .allowedMethods("GET", "POST", "PUT", "DELETE")
+    //             .allowedHeaders("*")
+    //             .maxAge(3600);
+    // }
 }
